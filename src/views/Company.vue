38aa7b7798ff6dd200a379b5e5c356ce35337b68
m.navarro <m.navarro@payvision.com>
2019-01-27 19:43:18 +0100

    New company view

---
+++ src/views/Company.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout
+    align-start
+    justify-center
+    row
+    fill-height
+  >
+    <v-flex
+      xs12
+      sm8
+    >
+      <v-card-business-form
+        :company="{}"
+        @onClose="closeForm"
+      />
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import VCardBusinessForm from '../components/VCardBusinessForm'
+
+export default {
+  components: {
+    VCardBusinessForm,
+  },
+  methods: {
+    closeForm() {
+      this.$router.push({ name: 'home' })
+    },
+  },
+}
+</script>
