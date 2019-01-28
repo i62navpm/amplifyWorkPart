7596f629a11383c1cd340538545c7352d1989529
m.navarro <m.navarro@payvision.com>
2019-01-28 21:24:05 +0100

    Added card business form

--- src/components/VCardBusinessForm.vue
+++ src/components/VCardBusinessForm.vue
@@ -20,7 +20,98 @@
 
     <v-divider />
 
-    <v-card-text style="height: 200px;" />
+    <v-card-text>
+      <v-form
+        ref="form"
+        v-model="valid"
+        @submit.prevent
+        @keyup.enter.native="save"
+      >
+        <v-container>
+          <v-layout wrap>
+            <amplify-photo-picker :photo-picker-config="photoPickerConfig" />
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="company.name"
+                :rules="[rules.required]"
+                label="Name"
+                required
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="company.cif"
+                :rules="[rules.required, rules.cif]"
+                label="Cif"
+                mask="A-########"
+                validate-on-blur
+                required
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="company.address"
+                label="Dirección"
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="company.phone"
+                label="Teléfono"
+                mask="#########"
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="company.email"
+                label="Email"
+                type="email"
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="company.web"
+                label="Web"
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              class="text-xs-right"
+            >
+              <v-btn
+                :loading="loading"
+                flat
+                color="primary"
+                @click="save"
+              >
+                <v-icon class="mr-2">
+                  save
+                </v-icon>
+                Guardar
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-form>
+    </v-card-text>
   </v-card>
 </template>
 
@@ -32,6 +123,25 @@ export default {
       type: Object,
       required: true,
       default: () => ({}),
+    },
+  },
+  data: () => ({
+    valid: false,
+    loading: false,
+    rules: {
+      required: value => !!value || 'Este campo es obligatorio.',
+      cif: value =>
+        /\D\d{8}/.test(value) || 'El CIF debe tener un formato correcto.',
+    },
+  }),
+  methods: {
+    save() {
+      if (!this.$refs.form.validate()) {
+        return
+      }
+      this.loading = true
+      console.log(this.company)
+      this.loading = false
     },
   },
 }
