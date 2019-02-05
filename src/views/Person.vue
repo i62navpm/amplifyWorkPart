cc8e7ca0c2ef94d60c754e78f0f6f8512c25aa8f
m.navarro <m.navarro@payvision.com>
2019-02-05 21:01:06 +0100

    Added person container view component

---
+++ src/views/Person.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-layout
+    align-start
+    justify-center
+    row
+    fill-height
+  >
+    <v-flex
+      xs12
+      sm8
+    >
+      <v-card-person-form
+        :person="{}"
+        @onClose="closeForm"
+        @onSubmit="saveForm"
+      />
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import VCardPersonForm from '../components/VCardPersonForm'
+
+export default {
+  components: {
+    VCardPersonForm,
+  },
+  methods: {
+    closeForm() {
+      this.$router.push({ name: 'people' })
+    },
+    saveForm(data) {
+      console.log(data)
+    },
+  },
+}
+</script>
