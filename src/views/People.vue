5feb3f55e2632cc9d7f59b2a9dc4930368a3fd88
m.navarro <m.navarro@payvision.com>
2019-02-05 21:00:57 +0100

    Added people container view component

---
+++ src/views/People.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout
+    align-start
+    justify-center
+    row
+    fill-height
+  >
+    <v-card-person
+      class="mx-1"
+      :person="{}"
+    />
+  </v-layout>
+</template>
+
+<script>
+import VCardPerson from '../components/VCardPerson'
+
+export default {
+  components: {
+    VCardPerson,
+  },
+}
+</script>
