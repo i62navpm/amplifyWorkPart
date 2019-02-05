87314db7efc6028ee6055585d041b7ea7913d1b6
m.navarro <m.navarro@payvision.com>
2019-02-05 21:01:37 +0100

    Added person components

---
+++ src/components/VCardPerson.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-card
+    class="mx-auto my-4"
+    max-width="400"
+  >
+    <v-card-title
+      primary-title
+      style="position: relative;"
+      class="pb-0"
+    >
+      <v-btn
+        absolute
+        color="primary"
+        class="white--text"
+        fab
+        right
+        top
+        :to="{name: 'people'}"
+      >
+        <v-icon>calendar_today</v-icon>
+      </v-btn>
+
+      <v-layout
+        class="mt-3"
+        align-center
+        justify-space-around
+        wrap
+      >
+        <v-flex xs2>
+          <v-avatar>
+            <img
+              :src="avatar"
+              alt="John"
+            >
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs10>
+          <h3 class="headline mb-0">
+            Kangaroo Valley Safari
+          </h3>
+          <div>Located two hours south of Sydney in the Southern Highlands of New South Wales, ...</div>
+        </v-flex>
+      </v-layout>
+    </v-card-title>
+
+    <v-card-actions>
+      <v-spacer />
+      <v-btn
+        flat
+        icon
+        color="red"
+        @click.native="removePerson"
+      >
+        <v-icon>delete</v-icon>
+      </v-btn>
+      <v-btn
+        flat
+        icon
+        :to="{name: 'person'}"
+        color="green"
+      >
+        <v-icon>edit</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import Identicon from 'identicon.js'
+
+export default {
+  name: 'VCardPerson',
+  props: {
+    person: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    avatar() {
+      const name = this.person.name || ''
+      return (
+        'data:image/png;base64,' +
+        new Identicon(name.padStart(15, '01'), 48).toString()
+      )
+    },
+  },
+  methods: {
+    removePerson() {
+      console.log(this.person)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.5;
+  position: absolute;
+  width: 100%;
+}
+</style>
+
---
+++ src/components/VCardPersonForm.vue
@@ -0,0 +1,193 @@
+<template>
+  <v-card class="card--flex-toolbar">
+    <v-toolbar
+      card
+      prominent
+    >
+      <v-toolbar-title class="body-2 grey--text">
+        Empleado
+      </v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn
+        icon
+        @click.native="$emit('onClose')"
+      >
+        <v-icon>close</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-divider />
+
+    <v-card-text>
+      <v-form
+        ref="form"
+        v-model="valid"
+        @submit.prevent
+        @keyup.enter.native="save"
+      >
+        <v-container>
+          <v-layout
+            align-center
+            justify-center
+            wrap
+          >
+            <v-flex
+              xs12
+              md6
+            >
+              <v-upload-image @onUploadImage="saveImage" />
+            </v-flex>
+          </v-layout>
+          <v-layout wrap>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="person.name"
+                :rules="[rules.required]"
+                label="Nombre"
+                required
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="person.nif"
+                :rules="[rules.required, rules.nif]"
+                label="Nif"
+                mask="########-A"
+                validate-on-blur
+                required
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="person.phone"
+                label="Teléfono"
+                mask="#########"
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="person.email"
+                label="Email"
+                type="email"
+              />
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                v-model="person.address"
+                label="Dirección"
+              />
+            </v-flex>
+            <v-flex xs12>
+              <v-textarea
+                v-model="person.description"
+                auto-grow
+                box
+                label="Descripción"
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="person.fullSalary"
+                append-outer-icon="euro"
+                type="number"
+                label="Salario completo"
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="person.halfSalary"
+                append-outer-icon="euro"
+                type="number"
+                label="Salario reducido"
+              />
+            </v-flex>
+            <v-flex
+              xs12
+              class="text-xs-right"
+            >
+              <v-btn
+                :loading="loading"
+                flat
+                color="primary"
+                @click="save"
+              >
+                <v-icon class="mr-2">
+                  save
+                </v-icon>
+                Guardar
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import VUploadImage from './VUploadImage'
+
+export default {
+  name: 'VCardPersonForm',
+  components: {
+    VUploadImage,
+  },
+  props: {
+    person: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+  },
+  data: () => ({
+    valid: false,
+    loading: false,
+    rules: {
+      required: value => !!value || 'Este campo es obligatorio.',
+      nif: value =>
+        /\d{8}\D/.test(value) || 'El NIF debe tener un formato correcto.',
+    },
+  }),
+  methods: {
+    save() {
+      if (!this.$refs.form.validate()) {
+        return
+      }
+      this.loading = true
+      this.$emit('onSubmit', this.person)
+      this.loading = false
+    },
+    saveImage(url) {
+      this.person.image = url
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card--flex-toolbar {
+  z-index: 6;
+  margin-top: -64px;
+}
+</style>
+
