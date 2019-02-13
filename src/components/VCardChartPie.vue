1a179c77d8d02edd2c9a83a4067ff59800c2c611
m.navarro <m.navarro@payvision.com>
2019-02-13 21:03:44 +0100

    Added chart pie

---
+++ src/components/VCardChartPie.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card max-height="350">
+    <ve-pie
+      :data="data"
+      height="350px"
+    />
+  </v-card>
+</template>
+
+<script>
+import VePie from 'v-charts/lib/pie.common'
+
+export default {
+  name: 'VCardChartPie',
+  components: {
+    VePie,
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
+
