fbed0cd3182ee56f4b687da84e8fb9b1ab9248dd
m.navarro <m.navarro@payvision.com>
2019-02-13 21:48:40 +0100

    Added calendar component

---
+++ src/components/VCardCalendar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <full-calendar
+        :events="data"
+        :config="config"
+        default-view="month"
+      />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { FullCalendar } from 'vue-full-calendar'
+import 'fullcalendar/dist/fullcalendar.css'
+
+export default {
+  name: 'VCardChartCalendar',
+  components: {
+    FullCalendar,
+  },
+  props: {
+    data: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      config: {
+        locale: 'es',
+      },
+    }
+  },
+}
+</script>
+
+
