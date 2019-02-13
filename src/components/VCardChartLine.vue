e16a1d0062a1ef39c1e34380a5a1d7a47c0bc18c
m.navarro <m.navarro@payvision.com>
2019-02-13 21:03:13 +0100

    Added chart line

---
+++ src/components/VCardChartLine.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card max-height="350">
+    <ve-line
+      :data="data"
+      height="350px"
+    />
+  </v-card>
+</template>
+
+<script>
+import VeLine from 'v-charts/lib/line.common'
+
+export default {
+  name: 'VCardChartLine',
+  components: {
+    VeLine,
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
+
