367ce0e63a0987d06002145f4c8238136c41fc2f
m.navarro <m.navarro@payvision.com>
2019-02-14 21:32:38 +0100

    Added calendar events

--- src/components/VCardCalendar.vue
+++ src/components/VCardCalendar.vue
@@ -1,23 +1,33 @@
 <template>
-  <v-card>
-    <v-card-text>
-      <full-calendar
-        :events="data"
-        :config="config"
-        default-view="month"
-      />
-    </v-card-text>
-  </v-card>
+  <div>
+    <v-event-options
+      ref="eventOptions"
+      @onAccept="saveEvent"
+    />
+    <v-card>
+      <v-card-text>
+        <full-calendar
+          :events="data"
+          :config="config"
+          default-view="month"
+          @event-selected="eventSelected"
+          @event-created="eventCreated"
+        />
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
 import { FullCalendar } from 'vue-full-calendar'
+import VEventOptions from './VEventOptions'
 import 'fullcalendar/dist/fullcalendar.css'
 
 export default {
   name: 'VCardChartCalendar',
   components: {
     FullCalendar,
+    VEventOptions,
   },
   props: {
     data: {
@@ -29,9 +39,27 @@ export default {
   data() {
     return {
       config: {
+        header: { left: 'title', center: '', right: 'today prev,next' },
+        buttonText: { today: 'Hoy' },
         locale: 'es',
       },
     }
+  },
+  methods: {
+    eventSelected(event, element) {
+      this.$refs.eventOptions.open = true
+      console.log('eventSelected')
+    },
+    eventCreated() {
+      console.log('eventCreated')
+    },
+    saveEvent(callback) {
+      return new Promise(success => {
+        setTimeout(() => {
+          success(callback())
+        }, 4000)
+      })
+    },
   },
 }
 </script>
