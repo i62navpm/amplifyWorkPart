ad97cc22f4386dd7a1a3df52d8b172a617309108
m.navarro <m.navarro@payvision.com>
2019-02-17 12:17:20 +0100

    Added breadcrums component

---
+++ src/components/TheBreadcrums.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-breadcrumbs :items="items">
+    <v-icon slot="divider">
+      chevron_right
+    </v-icon>
+    <template
+      slot="item"
+      slot-scope="props"
+    >
+      <li>
+        <router-link
+          :to="{name: props.item.href}"
+          class="v-breadcrumbs__item"
+          :class="[props.item.disabled && 'v-breadcrumbs__item--disabled']"
+        >
+          {{ props.item.text }}
+        </router-link>
+      </li>
+    </template>
+  </v-breadcrumbs>
+</template>
+
+<script>
+export default {
+  name: 'TheBreadcrums',
+  props: {
+    breadcrumbs: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      routes: {
+        business: {
+          text: 'Empresas',
+          href: 'home',
+        },
+        people: {
+          text: 'Trabajadores',
+          href: 'people',
+        },
+        worker: {
+          text: 'Trabajador',
+          href: 'worker',
+        },
+      },
+    }
+  },
+  computed: {
+    items() {
+      return [
+        ...this.breadcrumbs.map(breadcrumb => this.routes[breadcrumb]),
+        { ...this.routes[this.$route.name], disabled: true },
+      ]
+    },
+  },
+}
+</script>
