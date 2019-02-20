20437afb7c7cba7f833ad058d14cc9e904d3023f
m.navarro <m.navarro@payvision.com>
2019-02-20 20:30:52 +0100

    Added chart histogram

---
+++ src/components/VCardChartHistogram.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card max-height="380">
+    <v-card-text>
+      <ve-histogram :data="data" />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import VeHistogram from 'v-charts/lib/histogram.common'
+
+export default {
+  name: 'VCardChartHistogram',
+  components: {
+    VeHistogram,
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
+
