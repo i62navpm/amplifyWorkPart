2e5cd38b2c734b79e18a66586d5974a41de7fdea
m.navarro <m.navarro@payvision.com>
2019-02-20 20:31:00 +0100

    New workPdf component

---
+++ src/components/VCardPdf.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-card-text>
+      Pdf
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'VCardPdf',
+  data() {
+    return {
+      date: new Date().toISOString().substr(0, 7),
+    }
+  },
+}
+</script>
+
+
