949e2c34cbc2ca3eaa0cc93560824ac3c40d07a5
m.navarro <m.navarro@payvision.com>
2019-03-16 19:13:21 +0100

    New loading state

--- src/store/modules/toolbar.js
+++ src/store/modules/toolbar.js
@@ -1,13 +1,18 @@
 export default {
   state: {
     extended: false,
+    loading: false,
   },
   mutations: {
-    setExtended: (state, value) => {
-      state.extended = value
-    },
+    setExtended: (state, value) => (state.extended = value),
+    setLoading: (state, value) => (state.loading = value),
+  },
+  actions: {
+    startLoading: ({ commit }) => commit('setLoading', true),
+    stopLoading: ({ commit }) => commit('setLoading', false),
   },
   getters: {
     getExtended: state => state.extended,
+    getLoading: state => state.loading,
   },
 }
