8a4e99dbaf00075fad3d8596683e2653313be11c
m.navarro <m.navarro@payvision.com>
2019-03-17 18:49:27 +0100

    New card debt deatails component

---
+++ src/components/VCardDebtDetail.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card>
+    <v-toolbar
+      color="red"
+      dark
+    >
+      <v-toolbar-title>
+        Deuda
+      </v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn icon>
+        <v-icon>euro_symbol</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-card-text>
+      <div class="display-1 font-weight-light text-xs-center pa-2">
+        <strong class="red--text text--darken-2">
+          {{ debt }} €
+        </strong>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'VCardDebtDetail',
+  props: {
+    debt: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+}
+</script>
+
+
