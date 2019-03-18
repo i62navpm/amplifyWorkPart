c35b031861e77f8ad661bb20c26c3e31279491d1
m.navarro <m.navarro@payvision.com>
2019-03-18 20:39:56 +0100

    New notification component

---
+++ src/components/TheNotification.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-snackbar
+    v-model="notification.show"
+    :color="notification.color"
+    multi-line
+  >
+    {{ notification.message }}
+    <v-btn
+      dark
+      flat
+      @click="$store.dispatch('closeNotification')"
+    >
+      Cerrar
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: 'TheNotification',
+  computed: {
+    notification() {
+      return this.$store.getters.getNotification
+    },
+  },
+}
+</script>
