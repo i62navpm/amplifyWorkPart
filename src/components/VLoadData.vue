49e7bc612989496d5b7b5deb6fbcd37b3530860a
m.navarro <m.navarro@payvision.com>
2019-07-05 12:38:21 +0200

    New load data component

---
+++ src/components/VLoadData.vue
@@ -0,0 +1,48 @@
+<template>
+  <transition
+    transition
+    name="fade"
+    mode="out-in"
+  >
+    <div
+      v-if="loading"
+      key="loading"
+    >
+      <div class="title font-weight-light text-xs-center py-5">
+        <slot name="loading">
+          Cargando...
+        </slot>
+      </div>
+    </div>
+
+    <div
+      v-else-if="showNoResults"
+      key="noResults"
+    >
+      <div class="title font-weight-light text-xs-center py-5">
+        <slot name="noData">
+          Aun no hay nada creado
+        </slot>
+      </div>
+    </div>
+
+    <slot v-else />
+</transition>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    showNoResults: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+}
+</script>
