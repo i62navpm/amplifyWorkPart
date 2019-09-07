89603d57ed149a26352a36fb2c711048e331d7c7
m.navarro <m.navarro@payvision.com>
2019-09-07 11:58:43 +0200

    feat: persist dates in local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import auth from './modules/auth'
 import toolbar from './modules/toolbar'
 import notification from './modules/notification'
@@ -8,6 +9,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
+  plugins: [createPersistedState()],
   modules: {
     auth,
     toolbar,
